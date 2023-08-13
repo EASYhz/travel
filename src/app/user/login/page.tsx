b0d3315d07bf4d42229d2f342816deff560f1cf3
easyhz <jiton3@naver.com>
2023-08-13 11:47:28 +0900

    Update: 로그인 페이지 UI v1

---
+++ src/app/user/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginBox from "@/components/user/login/loginBox";
+
+export default function Login() {
+  return (
+    <div className="w-full h-full">
+      <LoginBox></LoginBox>
+    </div>
+  );
+}
---
+++ src/components/user/login/loginBox.tsx
@@ -0,0 +1,66 @@
+import Logo from "@/components/logo/logo";
+import Link from "next/link";
+
+export default function LoginBox() {
+  return (
+    <div>
+      {/* <section className="bg-gray-50 dark:bg-gray-900"> */}
+      <section className="bg-main-white">
+        <div className="flex flex-col items-center px-6 py-8 mx-auto md:h-screen lg:py-28">
+          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 ">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+                Login
+              </h1>
+              <form className="space-y-4 md:space-y-6" action="#">
+                <div>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                    placeholder="name@company.com"
+                    required
+                  />
+                </div>
+                <div>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="••••••••"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                    required
+                  />
+                </div>
+                <div className="flex items-center justify-between">
+                  <a
+                    href="#"
+                    className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
+                  >
+                    Forgot password?
+                  </a>
+                </div>
+                <button
+                  type="submit"
+                  className="w-full text-white bg-main-blue hover:bg-etc-blue focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                >
+                  Sign in
+                </button>
+                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                  Don’t have an account yet?{" "}
+                  <Link
+                    href="/user/join"
+                    className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                  >
+                    Sign up
+                  </Link>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
