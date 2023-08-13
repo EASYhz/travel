beda615750acc5d517cc16e51ff20ebb0302707b
easyhz <jiton3@naver.com>
2023-08-14 00:04:42 +0900

    Fix: warning -> Link 컴포넌트 css 분리 및 버전 다운로드

--- src/components/layout/mainHeader.tsx
+++ src/components/layout/mainHeader.tsx
@@ -5,25 +5,25 @@ export default function MainHeader() {
   return (
     <div className="flex w-full justify-between items-center h-16 fixed overflow-hidden px-[98px] py-2 bg-white border-t-0 border-r-0 border-b border-l-0 border-[#dee5ed]">
       {/* TODO : border 색상 변경 */}
-      <Link
-        href="/"
-        className="flex-grow-0 items-center flex-shrink-0 w-[109px] h-6"
-      >
-        <p className="absolute left-[98px] text-xl font-bold text-left text-[#7b95b7]">
-          Title
-        </p>
+      <Link href="/">
+        <div className="flex-grow-0 items-center flex-shrink-0 w-[109px] h-6">
+          <p className="absolute left-[98px] text-xl font-bold text-left text-[#7b95b7]">
+            Title
+          </p>
+        </div>
       </Link>
       <div className="flex justify-start items-center flex-grow-0 flex-shrink-0 relative gap-12">
-        <p className="header-menu">Home</p>
+        <Link href="/">
+          <p className="header-menu">Home</p>
+        </Link>
         <p className="header-menu">About</p>
-        <Link
-          href="/user/login"
-          className="flex justify-center items-center flex-grow-0 flex-shrink-0 relative overflow-hidden px-3 py-1 rounded-2xl bg-main-blue border border-main-blue"
-        >
+        <Link href="/user/login">
           {/* 로그인 상태 -> 로그아웃이나 마이페이지, 비로그인 상태 -> 로그인 상태 */}
-          <p className="flex-grow-0 flex-shrink-0 text-base text-left text-main-white">
-            로그인
-          </p>
+          <span className="flex justify-center items-center flex-grow-0 flex-shrink-0 relative overflow-hidden px-3 py-1 rounded-2xl bg-main-blue border border-main-blue">
+            <p className="flex-grow-0 flex-shrink-0 text-base text-left text-main-white">
+              로그인
+            </p>
+          </span>
         </Link>
       </div>
     </div>
--- src/components/user/login/loginBox.tsx
+++ src/components/user/login/loginBox.tsx
@@ -1,6 +1,7 @@
 import Logo from "@/components/logo/logo";
 import { Menu } from "@/interfaces/user/login";
 import Link from "next/link";
+import LoginForm from "./loginForm";
 import SocialLoginButtons from "./socialLoginButtons";
 
 export default function LoginBox() {
@@ -13,17 +14,19 @@ export default function LoginBox() {
     },
     {
       menuType: "회원가입",
+      linkUrl: "/user/join",
     },
   ];
   const loginMenu: JSX.Element[] = loginMenuList.map((el, index) => (
     <div key={index}>
-      <a
-        href="#"
-        className="text-sm mx-3 font-medium text-primary-600 hover:underline "
+      <Link
+        href={el.linkUrl !== undefined ? el.linkUrl : "#"}
         key={el.menuType}
       >
-        {el.menuType}
-      </a>
+        <span className="text-sm mx-3 font-medium text-primary-600 hover:underline ">
+          {el.menuType}
+        </span>
+      </Link>
       {index !== loginMenuList.length - 1 ? (
         <div className="divider" />
       ) : (
@@ -41,49 +44,14 @@ export default function LoginBox() {
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
                 Login
               </h1>
-              <form className="space-y-4 md:space-y-6" action="#">
-                <div>
-                  <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
-                    placeholder="이메일"
-                    required
-                  />
-                </div>
-                <div>
-                  <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    placeholder="비밀번호"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
-                    required
-                  />
-                </div>
-                <div className="flex items-center justify-center">
-                  {loginMenu}
-                </div>
-                <button type="submit" className="user-button">
-                  로그인
-                </button>
-                <div className="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-main-blue after:mt-0.5 after:flex-1 after:border-t after:border-main-blue">
-                  <p className="mx-4 mb-0 text-center font-semibold">OR</p>
-                </div>
-
-                <SocialLoginButtons />
-
-                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                  Don’t have an account yet?{" "}
-                  <Link
-                    href="/user/join"
-                    className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                  >
-                    Sign up
-                  </Link>
-                </p>
-              </form>
+              <LoginForm />
+              <div className="flex items-center justify-center">
+                {loginMenu}
+              </div>
+              <div className="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-main-blue after:mt-0.5 after:flex-1 after:border-t after:border-main-blue">
+                <p className="mx-4 mb-0 text-center font-semibold">OR</p>
+              </div>
+              <SocialLoginButtons />
             </div>
           </div>
         </div>
