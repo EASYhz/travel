d5480c984ed02bb949e0056a0f3bacdd60be82bf
easyhz <jiton3@naver.com>
2023-08-27 19:06:12 +0900

    Update: 카드뷰 레이아웃 수정

--- src/components/home/cardView.tsx
+++ src/components/home/cardView.tsx
@@ -23,9 +23,9 @@ export default function CardView(props: { imgId: number }) {
             /> */}
           </div>
           {/* <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-[hsla(0,0%,98%,0.15)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"></div> */}
-          <div className="p-6">
+          <div className="p-5">
             <h5 className="mb-2 text-xl font-medium leading-tight">Title</h5>
-            <p className="mb-4">This is..</p>
+            <p className="">This is..</p>
           </div>
         </div>
       </section>
