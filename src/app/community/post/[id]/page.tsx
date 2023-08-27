40d067364fcff6a829e05efa176f395f61d8feba
easyhz <jiton3@naver.com>
2023-08-27 19:05:43 +0900

    Update: post 화면 컴포넌트 세분화

--- src/app/community/post/[id]/page.tsx
+++ src/app/community/post/[id]/page.tsx
@@ -1,31 +1,15 @@
-import CommentBox from "@/components/community/post/commentBox";
-import CommentField from "@/components/community/post/commentField";
+import CommentField from "@/components/community/post/comment/commentField";
+import PostField from "@/components/community/post/postField";
+import CardView from "@/components/home/cardView";
 
 export default function Post(props: { params: { id: number } }) {
   const detailPost: string = "글 내용임 \n여기 추천합니다.";
+  const imgId: number = Number(props.params.id) + 1;
   return (
-    <div className="container mt-10 items-center justify-center mx-auto">
-      <h1 className="text-3xl">글 제목임</h1>
-      <div className="mt-6 text-xl whitespace-pre-line">{detailPost}</div>
-      <article className="mt-6">
-        {" "}
-        <img
-          className="rounded-t-lg"
-          src={`https://tecdn.b-cdn.net/img/new/standard/nature/${props.params.id}.jpg`}
-          alt=""
-        />
-      </article>
-      <article className="mt-6">
-        {" "}
-        <img
-          className="rounded-t-lg"
-          src={`https://tecdn.b-cdn.net/img/new/standard/nature/${props.params.id}.jpg`}
-          alt=""
-        />
-      </article>
-      <div className="main-divider mt-20" />
-      <div>
-        <CommentField />
+    <div className="flex">
+      <PostField params={props.params} />
+      <div className="w-1/5 py-10 hidden xl:block">
+        <CardView imgId={imgId} />
       </div>
     </div>
   );
---
+++ src/components/community/post/postField.tsx
@@ -0,0 +1,30 @@
+import CommentField from "./comment/commentField";
+
+export default function PostField(props: { params: { id: number } }) {
+  const detailPost: string = "글 내용임 \n여기 추천합니다.";
+
+  return (
+    <div className="container mt-10 items-center justify-center mx-auto">
+      <h1 className="text-3xl">글 제목임</h1>
+      <div className="mt-6 text-xl whitespace-pre-line">{detailPost}</div>
+      <article className="mt-6">
+        <img
+          className="rounded-t-lg"
+          src={`https://tecdn.b-cdn.net/img/new/standard/nature/${props.params.id}.jpg`}
+          alt=""
+        />
+      </article>
+      <article className="mt-6">
+        <img
+          className="rounded-t-lg"
+          src={`https://tecdn.b-cdn.net/img/new/standard/nature/${props.params.id}.jpg`}
+          alt=""
+        />
+      </article>
+      <div className="main-divider mt-20" />
+      <div>
+        <CommentField />
+      </div>
+    </div>
+  );
+}
