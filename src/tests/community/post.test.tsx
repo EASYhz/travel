ac62594defbac35df7eac1cf84f589431d55aa36
easyhz <jiton3@naver.com>
2023-08-29 00:10:20 +0900

    Test : post 테스트 코드 작성

---
+++ src/tests/community/post.test.tsx
@@ -0,0 +1,19 @@
+import Post from "@/app/community/post/[id]/page";
+import CommentField from "@/components/community/post/comment/commentField";
+import { render, screen } from "@testing-library/react";
+
+/* 게시글 화면 */
+describe("글 부분 ", () => {
+  it("제목", () => {
+    render(<Post params={{ id: 190 }} />);
+    expect(screen.getByText("글 제목임")).toBeInTheDocument();
+  });
+});
+
+describe("댓글 부분 ", () => {
+  it("댓글", () => {
+    render(<CommentField />);
+    expect(screen.getByText("댓글 (20)")).toBeInTheDocument();
+  });
+});
+export {};
