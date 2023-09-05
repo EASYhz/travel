3707a67a916f7c487ec2e5c26dd4cd0e54e62fa1
easyhz <jiton3@naver.com>
2023-09-05 22:35:02 +0900

    fix: 카드뷰 props 전달

--- src/app/community/post/[id]/page.tsx
+++ src/app/community/post/[id]/page.tsx
@@ -4,12 +4,22 @@ import CardView from "@/components/home/cardView";
 
 export default function Post(props: { params: { id: number } }) {
   const detailPost: string = "글 내용임 \n여기 추천합니다.";
+
   const imgId: number = Number(props.params.id) + 1;
   return (
     <div className="flex">
       <PostField params={props.params} />
       <div className="w-1/5 py-10 hidden xl:block">
-        <CardView imgId={imgId} />
+        <CardView
+          post={{
+            id: "1",
+            title: "Title1",
+            detail: "This is Detail",
+            user: "Alex",
+            imgId: 189,
+            regDt: "23.08.08 17:23",
+          }}
+        />
       </div>
     </div>
   );
